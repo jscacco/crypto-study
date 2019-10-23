118dcb1996b780a5e9b37060da9e1e0933b7ccb1
Jack Scacco <jscacco@hamilton.edu>
2019-10-22 22:01:44 -0400

    Added project4 skeleton from gemini.

---
+++ project4/df.cpp
@@ -0,0 +1,14 @@
+// Name: Jack Scacco
+// Due Date: 10.25.19
+// File: df.cpp
+// Purpose: Implement Diffie-Hellman key exchange
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello!" << endl;
+  return 0;
+}
+  
